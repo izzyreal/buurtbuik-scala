54041dbca6338dff1289ccf00333261ca4d552d4
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-09-04 11:12:00 +0200

    Add check against permission repo

--- src/main/scala/nl/buurtbuik/Authentication.scala
+++ src/main/scala/nl/buurtbuik/Authentication.scala
@@ -3,32 +3,36 @@ package nl.buurtbuik
 import cats.data.{Kleisli, OptionT}
 import cats.effect.IO
 import cats.implicits._
+import nl.buurtbuik.repository.PermissionRepository
 import org.http4s.dsl.io._
 import org.http4s.headers.Authorization
 import org.http4s.server.AuthMiddleware
 import org.http4s.{AuthedRoutes, BasicCredentials, Request}
 
-case class User(user: String, pass: String)
+case class User(user: String)
 
 object Authentication {
 
   private def retrieveUser: Kleisli[IO, BasicCredentials, User] = Kleisli(credentials => IO({
-    User(credentials.username, credentials.password)
+    User(credentials.username)
   }))
 
-  def validateBasicAuthHeader(header: String): Option[BasicCredentials] = {
+  def validate(header: String): Option[BasicCredentials] = {
     val payload = header.split(" ").lastOption
     if (payload.isDefined) {
-      Option(BasicCredentials(payload.get))
-    } else {
-      Option.empty
+      val credentials = BasicCredentials(payload.get)
+      val permission = PermissionRepository.getPermission(credentials.username, credentials.password)
+      if (permission.isDefined) {
+        return Option(credentials)
+      }
     }
+      Option.empty
   }
 
   private val authUser: Kleisli[IO, Request[IO], Either[String, User]] = Kleisli({ request =>
     val credentials = for {
       header <- request.headers.get(Authorization).toRight("Couldn't find an Authorization header")
-      credentials <- validateBasicAuthHeader(header.value).toRight("Invalid token")
+      credentials <- validate(header.value).toRight("Authorization header validation failed")
     } yield credentials
     credentials.traverse(retrieveUser.run)
   })

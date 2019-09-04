86c6455749a45a612c13c37ebb5ece06c5983d16
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-09-04 10:52:59 +0200

    Enable and configure CORS

--- src/main/scala/nl/buurtbuik/Application.scala
+++ src/main/scala/nl/buurtbuik/Application.scala
@@ -8,8 +8,10 @@ import nl.buurtbuik.endpoint.LocationEndpoints
 import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze._
+import org.http4s.server.middleware.{CORS, CORSConfig}
 
 import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
 
 object Application extends IOApp {
 
@@ -18,7 +20,15 @@ object Application extends IOApp {
     "org.postgresql.Driver", "jdbc:postgresql://localhost:5432/postgres", "postgres", "docker"
   )
 
-  private val services = Authentication.middleware(LocationEndpoints.locationEndpoints)
+  val corsConfig = CORSConfig(
+    anyOrigin = true,
+    anyMethod = false,
+    allowedOrigins = Set("foo.com", "bar.com"),
+    allowedMethods = Some(Set("GET", "POST")),
+    allowCredentials = true,
+    maxAge = 1.day.toSeconds)
+
+  private val services = CORS(Authentication.middleware(LocationEndpoints.locationEndpoints), corsConfig)
   private val httpApp = Router("/" -> services).orNotFound
 
   def run(args: List[String]): IO[ExitCode] = {

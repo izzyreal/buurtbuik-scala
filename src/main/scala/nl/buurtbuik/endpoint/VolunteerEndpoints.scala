7a6301006c12e9ba494079ae3901c5a84e40ce32
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-09-04 16:29:10 +0200

    Add volunteer post and insert

--- src/main/scala/nl/buurtbuik/endpoint/VolunteerEndpoints.scala
+++ src/main/scala/nl/buurtbuik/endpoint/VolunteerEndpoints.scala
@@ -4,11 +4,15 @@ import cats.effect.IO
 import io.circe.syntax._
 import nl.buurtbuik.User
 import nl.buurtbuik.repository.VolunteerRepository
-import org.http4s.AuthedRoutes
+import nl.buurtbuik.repository.VolunteerRepository.Volunteer
+import org.http4s.{AuthedRoutes, EntityDecoder, EntityEncoder, HttpRoutes, Request}
 import org.http4s.circe._
 import org.http4s.dsl.io._
 
 object VolunteerEndpoints {
+
+  private implicit val decoder: EntityDecoder[IO, Volunteer] = jsonOf[IO, Volunteer]
+  private implicit val encoder: EntityEncoder[IO, Int] = jsonEncoderOf[IO, Int]
 
   val volunteerEndpoints: AuthedRoutes[User, IO] = AuthedRoutes.of {
 
@@ -18,6 +22,8 @@ object VolunteerEndpoints {
     case GET -> Root / "volunteers" / IntVar(id) as _ =>
       Helper.getById(id, VolunteerRepository.getById)
 
+    case authReq@POST -> Root / "volunteers" as user =>
+        authReq.req.as[Volunteer].flatMap(VolunteerRepository.insert).flatMap(Created(_))
   }
 
 }
--- src/main/scala/nl/buurtbuik/repository/VolunteerRepository.scala
+++ src/main/scala/nl/buurtbuik/repository/VolunteerRepository.scala
@@ -7,11 +7,23 @@ import nl.buurtbuik.Application
 object VolunteerRepository {
 
   @JsonCodec
-  case class Volunteer(id: Int, email: String, firstName: String, lastName: String, phone: String)
+  case class Volunteer(id: Int,
+                       email: String,
+                       rolesId: Int,
+                       firstName: String,
+                       lastName: String,
+                       phone: String,
+                       defaultLocationId: Int)
 
   def getById(id: Int): Option[Volunteer] =
-    sql"select * from buurtbuik.volunteers where id = $id".query[Volunteer].option.transact(Application.xa).unsafeRunSync()
+    sql"select * from buurtbuik.volunteers where id = $id"
+      .query[Volunteer].option.transact(Application.xa).unsafeRunSync()
 
   def getAll: List[Volunteer] =
     sql"select * from buurtbuik.volunteers".query[Volunteer].to[List].transact(Application.xa).unsafeRunSync()
+
+  def insert(v: Volunteer) =
+    sql"insert into buurtbuik.volunteers values (default, ${v.email}, 1, ${v.firstName}, ${v.lastName}, ${v.phone}, 1)"
+      .update.run.transact(Application.xa)
+
 }

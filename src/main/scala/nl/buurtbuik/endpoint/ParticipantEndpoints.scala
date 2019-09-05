4623ef6ca37b9a4a8c3ecebb6dd6b282b143caab
Izmar Verhage <izmar.verhage@lunatech.nl>
2019-09-05 13:39:06 +0200

    Fix post participant

--- src/main/scala/nl/buurtbuik/endpoint/ParticipantEndpoints.scala
+++ src/main/scala/nl/buurtbuik/endpoint/ParticipantEndpoints.scala
@@ -5,7 +5,6 @@ import io.circe.generic.JsonCodec
 import io.circe.syntax._
 import nl.buurtbuik.Application.intEncoder
 import nl.buurtbuik.User
-import nl.buurtbuik.repository.ParticipantRepository.Participant
 import nl.buurtbuik.repository.{ParticipantRepository, VolunteerRepository}
 import org.http4s.circe._
 import org.http4s.dsl.io._
@@ -16,7 +15,10 @@ object ParticipantEndpoints {
   @JsonCodec
   case class IsCurrentUserParticipatingResponse(result: Boolean, currentUserId: Int)
 
-  private implicit val decoder: EntityDecoder[IO, Participant] = jsonOf[IO, Participant]
+  @JsonCodec
+  case class ParticipantPostData(volunteerId: Int, eventId: Int)
+
+  private implicit val decoder: EntityDecoder[IO, ParticipantPostData] = jsonOf[IO, ParticipantPostData]
   private implicit val encoder: EntityEncoder[IO, IsCurrentUserParticipatingResponse] = jsonEncoderOf[IO, IsCurrentUserParticipatingResponse]
 
   val participantEndpoints: AuthedRoutes[User, IO] = AuthedRoutes.of {
@@ -25,11 +27,13 @@ object ParticipantEndpoints {
       Ok(ParticipantRepository.getByEventId(eventId).asJson)
 
     case authReq@POST -> Root / "participants" as user =>
-      authReq.req.as[Participant].flatMap(ParticipantRepository.insert).flatMap(Created(_))
+      authReq.req.as[ParticipantPostData].flatMap(ParticipantRepository.insert).flatMap(Created(_))
 
     case GET -> Root / "participants" / "is-current-user-participating" / IntVar(eventId) as user =>
       val volunteer = VolunteerRepository.getByEmail(user.user).get
       val p = ParticipantRepository.getByEventId(eventId).find(p => p.volunteerId == volunteer.id)
       Ok(IsCurrentUserParticipatingResponse(p.isDefined, volunteer.id))
+
+
   }
 }
--- src/main/scala/nl/buurtbuik/repository/ParticipantRepository.scala
+++ src/main/scala/nl/buurtbuik/repository/ParticipantRepository.scala
@@ -4,23 +4,24 @@ import cats.effect.IO
 import doobie.implicits._
 import io.circe.generic.JsonCodec
 import nl.buurtbuik.Application.xa
+import nl.buurtbuik.endpoint.ParticipantEndpoints.ParticipantPostData
 
 object ParticipantRepository {
 
   @JsonCodec
-  case class Participant(volunteerId: Int, volunteerName: String, eventId: Int)
+  case class ParticipantResponse(volunteerId: Int, volunteerName: String, eventId: Int)
 
-  def insert(p: Participant): IO[Int] =
+  def insert(p: ParticipantPostData): IO[Int] =
     sql"insert into buurtbuik.participants values (${p.volunteerId}, ${p.eventId})"
       .update.run.transact(xa)
 
-  def getAll: List[Participant] =
+  def getAll: List[ParticipantResponse] =
     sql"select * from buurtbuik.participants"
-      .query[Participant].to[List].transact(xa).unsafeRunSync()
+      .query[ParticipantResponse].to[List].transact(xa).unsafeRunSync()
 
-  def getByEventId(eventId: Int): List[Participant] =
+  def getByEventId(eventId: Int): List[ParticipantResponse] =
     sql"select buurtbuik.participants.volunteer_id, buurtbuik.participants.event_id, buurtbuik.volunteers.first_name, buurtbuik.volunteers.last_name from buurtbuik.participants inner join buurtbuik.volunteers on buurtbuik.volunteers.id = buurtbuik.participants.volunteer_id where event_id = $eventId"
-      .query[(Int, Int, String, String)].map(tupleToParticipant).to[List].transact(xa).unsafeRunSync()
+      .query[(Int, Int, String, String)].map(tupleToParticipantResponse).to[List].transact(xa).unsafeRunSync()
 
   def deleteByEventId(eventId: Int): Unit =
     sql"delete from buurtbuik.participants where event_id = $eventId"
@@ -30,6 +31,6 @@ object ParticipantRepository {
     sql"delete from buurtbuik.participants where event_id = $eventId and volunteer_id = $volunteerId"
       .update.run.transact(xa).unsafeRunSync()
 
-  private def tupleToParticipant(t: (Int, Int, String, String)) =
-    Participant(t._1, t._3 + " " + t._4, t._2)
+  private def tupleToParticipantResponse(t: (Int, Int, String, String)) =
+    ParticipantResponse(t._1, t._3 + " " + t._4, t._2)
 }
